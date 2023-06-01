0e0ec932b750c2205d1d8523c9f92fce60a10524
Alexander Kushnir <dj.egregor1@gmail.com>
2023-06-01 21:39:13 +0200

    Update Tweet.jsx

--- src/components/Tweet/Tweet.jsx
+++ src/components/Tweet/Tweet.jsx
@@ -9,21 +9,10 @@ const Tweet = () => {
         <div className={styles.tweets}> 777 tweets</div>
         <div className={styles.followers}>100,500 Followers</div>
         <div className={styles.followChild} />
-        <img className={styles.boyIcon} alt="" src="/boy.svg" />
-        <img className={styles.picture21Icon} alt="" src="/picture2-1@2x.png" />
-        <img className={styles.logoIcon} alt="" src="/logo.svg" />
+        <img className={styles.boyIcon} alt="" src="./boy.svg" />
+        <img className={styles.picture21Icon} alt="" src="./picture2-1@2x.png" />
+        <img className={styles.logoIcon} alt="" src="./logo.svg" />
       </button>
-      {/* <div className={styles.following}>
-        <div className={styles.tweets1}> 777 tweets</div>
-        <div className={styles.followers1}>100,501 Followers</div>
-        <div className={styles.followChild} />
-        <img className={styles.boyIcon} alt="" src="/boy1.svg" />
-        <img className={styles.picture21Icon} alt="" src="/picture2-1@2x.png" />
-        <img className={styles.logoIcon} alt="" src="/logo1.svg" />
-        <div className={styles.button1}>
-          <div className={styles.following1}>Following</div>
-        </div>
-      </div> */}
     </div>
   );
 };
