91342def74765f93a20a46773ae618c116137ee1
Alexander Kushnir <dj.egregor1@gmail.com>
2023-06-03 08:11:01 +0200

    add update count  tweets functions

--- src/components/Tweet/Tweet.jsx
+++ src/components/Tweet/Tweet.jsx
@@ -1,14 +1,92 @@
+// import styles from './tweet.module.css';
+// const Tweet = ({ tweet }) => {
+//   return (
+//     <li className={styles.followParent}>
+//       <div className={styles.follow}>
+//         <button className={styles.button}>
+//           Follow
+//           {/* <div className={styles.follow1}>Follow</div> */}
+//         </button>
+//         <div className={styles.tweets}> {tweet.tweets} tweets</div>
+//         <div className={styles.followers}>{tweet.folovers} Followers</div>
+//         <div className={styles.followChild} />
+
+//         <div className={styles.outerRing}>
+//           <div className={styles.innerRing}></div>
+//         </div>
+
+//         <img className={styles.boyIcon} alt="" src={tweet.avatar} />
+//         <img className={styles.picture21Icon} alt="" src="./picture2-1@2x.png" />
+//         <img className={styles.logoIcon} alt="" src="./logo.svg" />
+//       </div>
+//     </li>
+//   );
+// };
+
+// export default Tweet;
+
+// import { useState } from 'react';
+// import styles from './tweet.module.css';
+
+// const Tweet = ({ tweet, handleFollowClick }) => {
+//   const [isFollowing, setIsFollowing] = useState(tweet.following);
+
+//   const handleClick = () => {
+//     if (!isFollowing) {
+//       handleFollowClick(tweet.id);
+//       setIsFollowing(true);
+//     }
+//   };
+
+//   return (
+//     <li className={styles.followParent}>
+//       <div className={styles.follow}>
+//         <button className={isFollowing ? `${styles.button} ${styles.buttonFollowing}` : styles.button} onClick={handleClick}>
+//           {isFollowing ? 'Following' : 'Follow'}
+//         </button>
+//         <div className={styles.tweets}> {tweet.tweets} tweets</div>
+//         <div className={styles.followers}>{tweet.followers} Followers</div>
+//         <div className={styles.followChild} />
+
+//         <div className={styles.outerRing}>
+//           <div className={styles.innerRing}></div>
+//         </div>
+
+//         <img className={styles.boyIcon} alt="" src={tweet.avatar} />
+//         <img className={styles.picture21Icon} alt="" src="./picture2-1@2x.png" />
+//         <img className={styles.logoIcon} alt="" src="./logo.svg" />
+//       </div>
+//     </li>
+//   );
+// };
+
+// export default Tweet;
+import { useState } from 'react';
 import styles from './tweet.module.css';
-const Tweet = ({ tweet }) => {
+import PropTypes from 'prop-types';
+
+const Tweet = ({ tweet, handleFollowClick, handleUnfollowClick }) => {
+  const [isFollowing, setIsFollowing] = useState(tweet.following);
+  console.log(tweet);
+
+  const handleButtonClick = async () => {
+    if (isFollowing) {
+      await handleUnfollowClick(tweet.id);
+      setIsFollowing(false);
+    } else {
+      await handleFollowClick(tweet.id);
+      setIsFollowing(true);
+    }
+  };
+
   return (
     <li className={styles.followParent}>
       <div className={styles.follow}>
-        <button className={styles.button}>
-          Follow
-          {/* <div className={styles.follow1}>Follow</div> */}
+        <button className={isFollowing ? `${styles.button} ${styles.buttonFollowing}` : styles.button} onClick={handleButtonClick}>
+          {isFollowing ? 'Following' : 'Follow'}
         </button>
         <div className={styles.tweets}> {tweet.tweets} tweets</div>
-        <div className={styles.followers}>{tweet.folovers} Followers</div>
+        <div className={styles.followers}>{tweet.followers} Followers</div>
         <div className={styles.followChild} />
 
         <div className={styles.outerRing}>
@@ -21,6 +99,17 @@ const Tweet = ({ tweet }) => {
       </div>
     </li>
   );
+};
+
+Tweet.propTypes = {
+  tweet: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    tweets: PropTypes.number.isRequired,
+    followers: PropTypes.number.isRequired,
+  }).isRequired,
+  handleFollowClick: PropTypes.func.isRequired,
+  handleUnfollowClick: PropTypes.func.isRequired,
 };
 
 export default Tweet;
--- src/components/TweetsList/TweetsList.jsx
+++ src/components/TweetsList/TweetsList.jsx
@@ -1,3 +1,63 @@
+// import { useState, useEffect, useCallback } from 'react';
+// import axios from 'axios';
+// import ReactLoading from 'react-loading';
+// import { ToastContainer, toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+// import Tweet from '../Tweet/Tweet';
+// import styles from './tweetList.module.css';
+
+// function TweetsList() {
+//   const [tweets, setTweets] = useState([]);
+//   const [page, setPage] = useState(1);
+//   const [isLastPage, setIsLastPage] = useState(false);
+
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [isError, setIsError] = useState(false);
+
+//   const fetchTweets = useCallback(async () => {
+//     try {
+//       setIsLoading(true);
+//       const response = await axios.get(`https://63ebe5f3be929df00ca4038a.mockapi.io/tweets?page=${page}&limit=3`);
+//       setTweets((prevTweets) => [...prevTweets, ...response.data]);
+//       if (response.data.length === 0) {
+//         setIsLastPage(true);
+//       }
+//     } catch (error) {
+//       console.error(error);
+//       setIsError(true);
+//       toast.error(error.message);
+//     } finally {
+//       setIsLoading(false);
+//     }
+//   }, [page]);
+
+//   const handleLoadMore = () => {
+//     setPage((prevPage) => prevPage + 1);
+//   };
+
+//   useEffect(() => {
+//     fetchTweets();
+//   }, [fetchTweets, page]);
+
+//   return (
+//     <div>
+//       <h1>Tweets</h1>
+//       <ToastContainer />
+//       {!isError && (
+//         <ul className={styles.tweetList}>
+//           {tweets.map((tweet) => (
+//             <Tweet key={tweet.id} tweet={tweet} />
+//           ))}
+//           {!isLastPage && !isLoading && <button onClick={handleLoadMore}>Load More</button>}
+//         </ul>
+//       )}
+
+//       {isLoading && <ReactLoading type="spin" color="#1538ff" height={40} width={40} />}
+//     </div>
+//   );
+// }
+// export default TweetsList;
+
 import { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 import ReactLoading from 'react-loading';
@@ -39,6 +99,42 @@ function TweetsList() {
     fetchTweets();
   }, [fetchTweets, page]);
 
+  const handleFollowClick = async (tweetId) => {
+    try {
+      const updatedTweets = tweets.map((tweet) => {
+        if (tweet.id === tweetId) {
+          return { ...tweet, followers: tweet.followers + 1, following: true };
+        }
+        return tweet;
+      });
+      setTweets(updatedTweets);
+      await axios.put(`https://63ebe5f3be929df00ca4038a.mockapi.io/tweets/${tweetId}`, {
+        followers: updatedTweets.find((tweet) => tweet.id === tweetId).followers,
+      });
+    } catch (error) {
+      console.error(error);
+      toast.error('Failed to update followers');
+    }
+  };
+
+  const handleUnfollowClick = async (tweetId) => {
+    try {
+      const updatedTweets = tweets.map((tweet) => {
+        if (tweet.id === tweetId) {
+          return { ...tweet, followers: tweet.followers - 1, following: false };
+        }
+        return tweet;
+      });
+      setTweets(updatedTweets);
+      await axios.put(`https://63ebe5f3be929df00ca4038a.mockapi.io/tweets/${tweetId}`, {
+        followers: updatedTweets.find((tweet) => tweet.id === tweetId).followers,
+      });
+    } catch (error) {
+      console.error(error);
+      toast.error('Failed to update followers');
+    }
+  };
+
   return (
     <div>
       <h1>Tweets</h1>
@@ -46,7 +142,7 @@ function TweetsList() {
       {!isError && (
         <ul className={styles.tweetList}>
           {tweets.map((tweet) => (
-            <Tweet key={tweet.id} tweet={tweet} />
+            <Tweet key={tweet.id} tweet={tweet} handleFollowClick={handleFollowClick} handleUnfollowClick={handleUnfollowClick} />
           ))}
           {!isLastPage && !isLoading && <button onClick={handleLoadMore}>Load More</button>}
         </ul>
@@ -56,4 +152,5 @@ function TweetsList() {
     </div>
   );
 }
+
 export default TweetsList;
